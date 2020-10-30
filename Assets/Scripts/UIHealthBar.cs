6b2ddc86daea74557a8257ff4dafea9ccd8e3a62
Katherine <396626@colum.edu>
2020-10-29 23:51:37 -0500

    Camera Cinemachine + HUB
    Camera seeting + Healthbar

---
+++ Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Console;
+
+public class UIHealthBar : MonoBehaviour
+{
+    public static UIHealthBar instance { get; private set; }
+
+    public Image mask;
+    float originalSize;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        originalSize = mask.rectTransform.rect.width;
+    }
+
+    public void SetValue(float value)
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+}
