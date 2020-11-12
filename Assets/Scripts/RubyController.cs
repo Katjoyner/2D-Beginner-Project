6d1ad0848bad9a9cdbb33b02326f970761f6e042
Katherine <396626@colum.edu>
2020-11-12 14:06:58 -0600

    Dialogue, audio, & build
    3 steps

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -12,6 +12,9 @@ public class RubyController : MonoBehaviour
 
     public GameObject projectilePrefab;
 
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+
     public int health {  get { return currentHealth; }}
     int currentHealth;
 
@@ -25,6 +28,7 @@ public class RubyController : MonoBehaviour
 
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +37,10 @@ public class RubyController : MonoBehaviour
         animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
+
+        audioSource = GetComponent<AudioSource>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -63,6 +70,18 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        } 
     }
 
     void FixedUpdate()
@@ -97,6 +116,11 @@ public class RubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+        PlaySound(throwSound);
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 
 }
